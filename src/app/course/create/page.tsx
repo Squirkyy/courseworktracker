df706fac3797e30f8655c91ec85b35e54fdab68e
Darius Vollmer <dariusv@outlook.de>
2023-11-21 03:00:41 +0100

    pain

--- src/app/course/create/page.tsx
+++ src/app/course/create/page.tsx
@@ -1,10 +1,16 @@
 function Page() {
     return (
-        <div className="h-screen flex flex-col items-center justify-center">
-            <div>
-            
+        <>
+            <div className="h-screen w-full flex flex-col items-center justify-center">
+                <div>
+                    <div className="flex w-full">
+                        <div className="grid h-20 flex-grow card bg-base-300 rounded-box place-items-center">Create from scratch</div>
+                        <div className="divider divider-horizontal">OR</div>
+                        <div className="grid h-20 flex-grow card bg-base-300 rounded-box place-items-center">Create from template</div>
+                    </div>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 export default Page;
